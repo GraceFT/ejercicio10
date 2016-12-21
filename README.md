39c8097628551e213c70caa81cdc09e4191cf5ca
GraceF <gracefctorres@gmail.com>
2016-12-21 10:41:29 -0500

    function-javascript

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# ejercicio10
-EjercicioNro10
+# Ejercicio10: as10
+
+Inicio: Evaluar dos numeros enteros.
+
+##Ingreso de Datos
+- a
+- b
+- evaluar
+
+##Proceso
+- Pedir a
+- Pedir b
+- Evaluar a y b y guardar en evaluar.
+    - Si (a = 10 o b = 10 o a + b =10)
+            retorna 1//si uno es igual a 10 o si ambos suman 10.
+        Si no 
+            retorna 0        
+    Fin si
+
+*Fin*
+
--- main.js
+++ main.js
@@ -1,21 +1,11 @@
    function evaluar(){
-    var parrot = document.getElementById("parrot").value;
-    var hora = parseFloat(document.getElementById("hora").value);
-    var isParrot = false;
-       
-    
+    var aNum = parseInt(document.getElementById("anum").value);
+    var bNum = parseInt(document.getElementById("bnum").value);
     var salida = document.getElementById("salida");
     
-    if (parrot == 1)
-        isParrot = true;
-    
-    if(hora >= 0 && hora <=23 && !isParrot){        document.getElementById("salida").innerHTML=" No estamos en problemas"; ;
+    if (aNum == 10 || bNum == 10 || aNum + bNum == 10){
+        document.getElementById("salida").innerHTML="1";
     }else{
-        if(hora < 7 && isParrot || hora > 20 && isParrot){
-            document.getElementById("salida").innerHTML="Estamos en problemas";
-        }
-        else{
-            document.getElementById("salida").innerHTML=" No estamos en problemas";
-        }
+        document.getElementById("salida").innerHTML="0";
     }
 }
