16d064a8c8d3d43236ba3e1fc7dcdda2aa35c8b5
GraceF <gracefctorres@gmail.com>
2016-12-21 12:45:08 -0500

    design

--- main.js
+++ main.js
@@ -4,7 +4,7 @@
     var salida = document.getElementById("salida");
     
     if (aNum == 10 || bNum == 10 || aNum + bNum == 10){
-        document.getElementById("salida").innerHTML="1";
+        document.getElementById("salida").innerHTML="Uno de los enteros es 10 o suman 10 : 1";
     }else{
         document.getElementById("salida").innerHTML="0";
     }
